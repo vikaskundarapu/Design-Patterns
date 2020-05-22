d92600195ad97c9a4cbf4139040b9b4a6eeb2035
Vikas Kundarapu <vikas.kundarapu@gmail.com>
2020-05-23 00:33:23 +0530

    Changed the formatting

--- src/behavioral/visitor/simple/Tree.java
+++ src/behavioral/visitor/simple/Tree.java
@@ -1,4 +1,5 @@
 package behavioral.visitor.simple;
+
 /*
  * Often, a data structure is defined by case analysis and recursion. 
  * For example, a binary tree of type Tree<E> is one of the following:
@@ -7,13 +8,11 @@ package behavioral.visitor.simple;
  */
 abstract class Tree<E> {
 
-	abstract public String toString();
-	abstract public Double sum();
-	
-	
-	public static <E> Tree<E> leaf(E e){
-		
-		return  new Tree<E>() {
+	public abstract Double sum();
+
+	public static <E> Tree<E> leaf(E e) {
+
+		return new Tree<E>() {
 
 			@Override
 			public String toString() {
@@ -22,24 +21,24 @@ abstract class Tree<E> {
 
 			@Override
 			public Double sum() {
-				return ((Number)e).doubleValue();
+				return ((Number) e).doubleValue();
 			}
 		};
 	}
-	
-	public static <E> Tree<E> branch(Tree<E> l, Tree<E> r){
+
+	public static <E> Tree<E> branch(Tree<E> l, Tree<E> r) {
 		return new Tree<E>() {
 
 			@Override
 			public String toString() {
-				return  "(" + l.toString() + "^" + r.toString() + ")";
+				return "(" + l.toString() + "^" + r.toString() + ")";
 			}
 
 			@Override
 			public Double sum() {
-				return l.sum()+ r.sum();
+				return l.sum() + r.sum();
 			}
-			
+
 		};
 	}
-	}
+}
